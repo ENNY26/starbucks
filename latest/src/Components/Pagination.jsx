aa30c457818a19f006f67c8df0bdc60e67194a53
eniola orehin <eniola.orehin@cix.csi.cuny.edu>
2025-02-15 02:03:46 -0500

    Add react-icons dependency and enhance Pagination component with toggle for displaying page numbers (pagination not  working as expected just yet)

--- latest/src/Components/Pagination.jsx
+++ latest/src/Components/Pagination.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Pagination = ({recipesPerPage, totalRecipes, paginate, curentPage}) => {
-    const pageNumbers = []
+const Pagination = ({ recipesPerPage, totalRecipes, paginate, currentPage }) => {
+    const [showAllPages, setShowAllPages] = useState(false);
+    const pageNumbers = [];
 
-    for(let i =1; i <= Math.ceil(totalRecipes /recipesPerPage); i++){
-        pageNumbers.push(i)
+    for (let i = 1; i <= Math.ceil(totalRecipes / recipesPerPage); i++) {
+        pageNumbers.push(i);
     }
-  return (
-    <nav>
-        <ul className='flex justify-center mt-4'>
-            {pageNumbers.map((number) =>
-            <li key={number} className='mx-2'>
-                <button onClick={() => paginate(number)}
-                className={`px-4 py-2 ${
-                    curentPage === number ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-700'
-                } rounded`}
-                >
-                    {number}
-                    
-                 </button>
-            </li>
-            )}
-        </ul>
-    </nav>
-  )
-}
 
-export default Pagination
+    const toggleShowAllPages = () => {
+        setShowAllPages(!showAllPages);
+    };
+
+    return (
+        <nav>
+            <ul className='flex justify-center mt-4'>
+                {pageNumbers.length > 5 && !showAllPages ? (
+                    <li className='mx-2'>
+                        <button
+                            onClick={toggleShowAllPages}
+                            className='px-4 py-2 bg-gray-200 text-gray-700 rounded'
+                        >
+                            &rarr;
+                        </button>
+                    </li>
+                ) : (
+                    pageNumbers.map((number) => (
+                        <li key={number} className='mx-2'>
+                            <button
+                                onClick={() => paginate(number)}
+                                className={`px-4 py-2 ${
+                                    currentPage === number ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-700'
+                                } rounded`}
+                            >
+                                {number}
+                            </button>
+                        </li>
+                    ))
+                )}
+            </ul>
+        </nav>
+    );
+};
+
+export default Pagination;
